7800bc927adad642df4e92323a27f1ca11c64dfc
shannongamby <shannongamby@hotmail.com>
2018-11-05 11:53:44 +0000

    fixed typo

--- database/02_create_property_table.sql
+++ database/02_create_property_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE property(id SERIAL PRIMARY KEY, user id VARCHAR(60), name VARCHAR(60), price VARCHAR(60), location VARCHAR(60), bedrooms NUMERIC, type VARCHAR(60), description VARCHAR(240));
+CREATE TABLE property(id SERIAL PRIMARY KEY, user_id VARCHAR(60), name VARCHAR(60), price VARCHAR(60), location VARCHAR(60), bedrooms NUMERIC, type VARCHAR(60), description VARCHAR(240));
