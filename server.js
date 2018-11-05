8205a65ba6df3f29d63160016996fe7be975b5fc
felixjtdb <felixjtdb@gmail.com>
2018-11-05 14:52:42 +0000

    adding ejs and express to dependencies

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 const PORT = "8080"
 const HOST = "127.0.0.1"
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-  res.write("hi")
+  res.write("hi");
   res.end()
 });
 
 app.get('/signup', (req, res) => {
-  res.write("signup")
+  res.render("../views/sign_up", {});
   res.end()
 });
 
