3e50d9ad6418324ee8693761a23ccc89a27a5922
shannongamby <shannongamby@hotmail.com>
2018-11-08 10:48:54 +0000

    failing log in test

--- spec/features/loggingInSpec.js
+++ spec/features/loggingInSpec.js
@@ -3,22 +3,18 @@ var Browser = require("zombie")
 Browser.localhost("localhost", 8080)
 
 describe('Logging in', function() {
+  console.log(1)
   var browser = new Browser();
   var server = require("../../server.js")
 
-  beforeEach(function(done) {
-    browser.visit('/')
-    browser.clickLink('#nav-link-login')
-
-    browser
-      .fill('#username', 't_user')
-      .fill('#password', 'qwerty')
-
-    return browser.pressButton('#login', done);
-  });
-
-  it('should displays username after logging in', function() {
-    browser.assert.text("h4", "Logged in as t_user")
+  it('should displays username after logging in', function(next) {
+    browser.visit("http://localhost:8080/login")
+    browser.fill('input[name="username"]', 'l_user')
+    browser.fill('input[name="password"]', 'l_user')
+    .pressButton('input[value="Login"]', function() {
+      expect(browser.html("body")).toContain("Logged in as t_user")
+      next()
+    })
   });
 
 });
